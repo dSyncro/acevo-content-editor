937c6259e08cd1ea046c399c145e940c4b1040ab
Domenico Mancini <mancini.domenico02@gmail.com>
2025-01-31 14:29:31 +0100

    feat: add async logging initialization function

--- src/functions/logging.rs
+++ src/functions/logging.rs
@@ -1,4 +1,11 @@
-pub fn init_logging(verbosity: u8) {
+use std::sync::Arc;
+
+use spdlog::{
+	sink::{AsyncPoolSink, OverflowPolicy},
+	Error, Logger,
+};
+
+pub fn init_logging(verbosity: u8) -> Result<Arc<Logger>, Error> {
 	let level = match verbosity {
 		0 => spdlog::Level::Warn,
 		1 => spdlog::Level::Info,
@@ -7,5 +14,18 @@ pub fn init_logging(verbosity: u8) {
 	};
 
 	let filter = spdlog::LevelFilter::MoreSevereEqual(level);
-	spdlog::default_logger().set_level_filter(filter);
+	let logger = spdlog::default_logger().fork_with(|new| {
+		let file_sink = Arc::new(
+			AsyncPoolSink::builder()
+				.overflow_policy(OverflowPolicy::Block)
+				.level_filter(filter)
+				.build()?,
+		);
+		new.sinks_mut().push(file_sink);
+		Ok(())
+	})?;
+
+	spdlog::set_default_logger(logger.clone());
+
+	Ok(logger)
 }

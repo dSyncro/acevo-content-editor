47c8b936fd241ed6a0658e77f15a30d68ee0b697
Domenico Mancini <mancini.domenico02@gmail.com>
2025-01-31 14:30:31 +0100

    feat: add some utility functions and structs

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,8 +2,10 @@ mod commands;
 mod file_entry;
 mod package_file_table;
 mod packed_package_buffer;
+mod path_tree;
 
 pub use commands::*;
 pub use file_entry::*;
 pub use package_file_table::*;
 pub use packed_package_buffer::*;
+pub use path_tree::*;
